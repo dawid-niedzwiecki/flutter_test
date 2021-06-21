932ccf94bc0d61737d1383330c265b1b556c12ea
dawid-niedzwiecki <dawid.niedwiecki@gmail.com>
2021-06-21 18:28:23 +0200

    asd

--- lib/screens/login-screen.dart
+++ lib/screens/login-screen.dart
@@ -15,7 +15,7 @@ class _LoginPageState extends State<LoginPage> {
       body: Stack(
         children: [
           Image(
-            image: AssetImage("burger.png"),
+            image: AssetImage("assets/burger.png"),
             fit: BoxFit.cover,
             height: double.infinity,
             width: double.infinity,
