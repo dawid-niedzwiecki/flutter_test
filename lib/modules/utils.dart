7f67f946c79ccc2cd3ea13e4604ff17dd94b2917
Szymon3125 <50416224+Szymon3125@users.noreply.github.com>
2021-06-21 14:55:09 +0200

    przyciski

--- lib/modules/utils.dart
+++ lib/modules/utils.dart
@@ -4,5 +4,6 @@ import 'package:flutter/cupertino.dart';
 class Utils {
   static Color backgroundColor = new Color(0xff404040);
   static Color primaryColor = new Color(0xffc23dff);
+  static Color secondaryColor = new Color(0xff202020);
   static Color textColor = new Color(0xffe0e0e0);
 }
