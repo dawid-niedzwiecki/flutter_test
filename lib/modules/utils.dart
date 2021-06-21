ddf669fbb4d21647bdb094bd0a5b44cc7527ad97
dawid-niedzwiecki <dawid.niedwiecki@gmail.com>
2021-06-21 23:36:39 +0200

    utilsy

--- lib/modules/utils.dart
+++ lib/modules/utils.dart
@@ -2,10 +2,10 @@ import 'package:flutter/cupertino.dart';
 
 // Klasa pomocnicza
 class Utils {
-  static Color backgroundColor = new Color(0xff404040);
-  static Color primaryColor = new Color(0xffc23dff);
-  static Color secondaryColor = new Color(0xff202020);
-  static Color textColor = new Color(0xffe0e0e0);
+  static Color backgroundColor = new Color(0xff343F56);
+  static Color primaryColor = new Color(0xfffb9300);
+  static Color secondaryColor = new Color(0xfff54748);
+  static Color textColor = new Color(0xfff5e6ca);
   static Text generateText(String value,
       {double fontSize = 15.0,
       Color color = const Color(0xffe0e0e0),
