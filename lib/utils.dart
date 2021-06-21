bfee5d19b763eb148aa2b519f86d8757b866d570
Szymon3125 <50416224+Szymon3125@users.noreply.github.com>
2021-06-21 12:15:31 +0200

    komentarz

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 
+// Klasa pomocnicza
 class Utils {
   static Color backgroundColor = new Color(0xff222222);
   static Color primaryColor = new Color(0xffff8080);
